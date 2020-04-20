6618bbcd5c408202a8fb4eaff2bf7f8589d4d7b7
DJLeemstar <47821066+DJLeemstar@users.noreply.github.com>
2020-04-20 14:35:23 +0100

    Add files via upload

---
+++ ScopeAndParameters.py
@@ -0,0 +1,70 @@
+#20/04/2020
+#ClassAndStructure
+
+#LGEB
+#LEGB
+#Scope
+
+# Local
+# Global
+# Enclosed
+# Built-In
+
+# Global
+var1 = 1
+var2 = 3
+var3 = 4
+
+x = 0
+
+
+def function(variable1, variable2):
+    # Local
+    # y = x
+    print(variable1)
+    global x
+    global abcdefghikl
+    print(x)
+    print(abcdefghikl)
+    print(variable2)
+
+# goto
+
+# global
+
+
+x = "abcdefghijkl"
+abcdefghikl = "x"
+
+function(var1, var2)
+
+'''
+def function(variable1, variable2):
+    print("This is a new function")
+
+
+function(var1, var2)
+
+
+def function(variable1):
+    print(variable1)
+
+
+function(var2)
+
+
+def function(variable1, variable2, variable3):
+    print(variable3)
+
+
+function(var1, var2, var3)
+'''
+
+# variable = 2
+# int variable = 2
+# float variable = 2.0
+
+
+
+# Encapsulation
+
